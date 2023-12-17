853a255a46ad54e12615e715ce0b1970ac47b3ea
TCoelhoUA <trs.coelho@ua.pt>
2023-12-17 16:13:25 +0000

    CLIENTE: Catálogo, Encomendas e Notificações

---
+++ cliente/notification.js
@@ -0,0 +1,9 @@
+var popup = document.getElementById("popup")
+
+function togglenotis() {
+    if (popup.style.display === "none") {
+        popup.style.display = "block"
+    } else {
+        popup.style.display = "none"
+    }
+    }
